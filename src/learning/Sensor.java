4f5de964982e3372abb42dc472c898f8dc9d9a6d
Victor-LR <victor.lelu_ribaimont@yahoo.fr>
2019-05-14 16:16:25 +0200

    sensor

---
+++ src/learning/Sensor.java
@@ -0,0 +1,39 @@
+package learning;
+
+import learning.perceptron.SparseVector;
+import agents.AgentAction;
+import map.GameState;
+
+public class Sensor 
+{
+	protected StateSensor state_sensor;
+	
+	public Sensor(StateSensor ss)
+	{
+		this.state_sensor = ss;
+	}
+	public int size()
+	{
+		return(state_sensor.size()*4+1);
+	}
+	
+	public SparseVector getVector(GameState GS, AgentAction action)
+	{
+		SparseVector vec1 = new SparseVector(size());
+		SparseVector vec2 =state_sensor.getVector(GS);
+		vec1.setValue(0,1.0);
+		int pos = action.getDirection() * state_sensor.size()+1;
+		
+		System.out.println("vec2");
+		System.out.println(vec2);
+		
+		for(int f : vec2){
+			r.setValue(pos+f,vec2.getValue(f));
+		}
+		
+		System.out.println("vec1");
+		System.out.println(vec1);
+		
+		return(vec1);
+	}
+}
---
+++ src/learning/StateSensor.java
@@ -0,0 +1,11 @@
+package learning;
+
+import learning.perceptron.SparseVector;
+import agents.AgentAction;
+import map.GameState;
+
+public interface StateSensor
+{
+	public int size();	
+	public SparseVector getVector(GameState GS);
+}
